d2af7727e697b3c7004880326e94eaa4bc62acb6
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2013-02-27 20:19:01 -0500

    fix boost build on mac

--- Projects/boost.cmake
+++ Projects/boost.cmake
@@ -2,7 +2,7 @@ add_external_project(boost
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND
     <SOURCE_DIR>/bootstrap.sh --prefix=<INSTALL_DIR>
-                              --with-libraries=date_time,thread
-  BUILD_COMMAND <SOURCE_DIR>/bjam
-  INSTALL_COMMAND <SOURCE_DIR>/bjam --prefix=<INSTALL_DIR> install
+                              --with-libraries=date_time,thread,regex,system
+  BUILD_COMMAND <SOURCE_DIR>/bjam address-model=64 threading=multi
+  INSTALL_COMMAND <SOURCE_DIR>/bjam  address-model=64 threading=multi --prefix=<INSTALL_DIR> install
 )
