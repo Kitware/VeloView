4921c46877d36ec6bf09e5f31e2e12f121162d69
Pat Marion <pat.marion@kitware.com>
2013-04-10 23:17:37 +1000

    Add new vtkVelodyneHDLGridSource to create a measurement grid

---
+++ VelodyneHDL/vtkVelodyneHDLGridSource.cxx
@@ -0,0 +1,118 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkVelodyneHDLGridSource.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "vtkVelodyneHDLGridSource.h"
+
+#include "vtkNew.h"
+#include "vtkSmartPointer.h"
+#include "vtkPlaneSource.h"
+#include "vtkArcSource.h"
+#include "vtkExtractEdges.h"
+#include "vtkAppendPolyData.h"
+#include "vtkPolyData.h"
+#include "vtkMath.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkObjectFactory.h"
+
+
+//-----------------------------------------------------------------------------
+vtkStandardNewMacro(vtkVelodyneHDLGridSource);
+
+//-----------------------------------------------------------------------------
+vtkVelodyneHDLGridSource::vtkVelodyneHDLGridSource()
+{
+  this->GridSize = 10;
+
+  this->Origin[0] = 0.0;
+  this->Origin[1] = 0.0;
+  this->Origin[2] = 0.0;
+
+  this->Normal[0] = 0.0;
+  this->Normal[1] = 0.0;
+  this->Normal[2] = 1.0;
+
+  this->SetNumberOfInputPorts(0);
+  this->SetNumberOfOutputPorts(1);
+}
+
+//-----------------------------------------------------------------------------
+vtkVelodyneHDLGridSource::~vtkVelodyneHDLGridSource()
+{
+}
+
+//-----------------------------------------------------------------------------
+vtkSmartPointer<vtkPolyData> vtkVelodyneHDLGridSource::CreateGrid(int gridSize, double origin[3], double normal[3])
+{
+  vtkNew<vtkPlaneSource> plane;
+  vtkNew<vtkExtractEdges> edges;
+  vtkNew<vtkAppendPolyData> append;
+
+  plane->SetOrigin(-gridSize, -gridSize, 0.0);
+  plane->SetPoint1(gridSize, -gridSize, 0.0);
+  plane->SetPoint2(-gridSize, gridSize, 0.0);
+  plane->SetResolution(gridSize*2, gridSize*2);
+  plane->SetCenter(origin);
+  plane->SetNormal(normal);
+
+  edges->SetInputConnection(plane->GetOutputPort());
+  append->AddInputConnection(edges->GetOutputPort());
+
+  double arcStartVector[3];
+  vtkMath::Perpendiculars (normal, arcStartVector, NULL, 0);
+
+  for (int i = 1; i <= gridSize; ++i)
+    {
+    double startPoint[3] = {arcStartVector[0]*i, arcStartVector[1]*i, arcStartVector[2]*i};
+    vtkNew<vtkArcSource> arc;
+    arc->UseNormalAndAngleOn();
+    arc->SetCenter(origin);
+    arc->SetPolarVector(startPoint);
+    arc->SetAngle(360);
+    arc->SetNormal(normal);
+    arc->SetResolution(360);
+    append->AddInputConnection(arc->GetOutputPort());
+    }
+
+  append->Update();
+  vtkSmartPointer<vtkPolyData> polyData = vtkSmartPointer<vtkPolyData>::New();
+  polyData->ShallowCopy(append->GetOutput());
+  return polyData;
+}
+
+//-----------------------------------------------------------------------------
+int vtkVelodyneHDLGridSource::RequestData(vtkInformation *request,
+                              vtkInformationVector **inputVector,
+                              vtkInformationVector *outputVector)
+{
+  vtkPolyData *output = vtkPolyData::GetData(outputVector);
+  vtkInformation *info = outputVector->GetInformationObject(0);
+
+  if (this->GridSize < 1)
+    {
+    vtkErrorMacro("Specified grid size " << this->GridSize << " is out of range.  Must be >= 1.");
+    return 0;
+    }
+
+  output->ShallowCopy(this->CreateGrid(this->GridSize, this->Origin, this->Normal));
+  return 1;
+}
+
+//-----------------------------------------------------------------------------
+void vtkVelodyneHDLGridSource::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+  os << indent << "GridSize: " << this->GridSize << endl;
+}
---
+++ VelodyneHDL/vtkVelodyneHDLGridSource.h
@@ -0,0 +1,61 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkVelodyneHDLGridSource.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkVelodyneHDLGridSource - generates a vtkPolyData measurement grid plane
+// .Section Description
+//
+
+#ifndef _vtkVelodyneHDLGridSource_h
+#define _vtkVelodyneHDLGridSource_h
+
+#include <vtkPolyDataAlgorithm.h>
+#include <vtkSmartPointer.h>
+
+class VTK_EXPORT vtkVelodyneHDLGridSource : public vtkPolyDataAlgorithm
+{
+public:
+  static vtkVelodyneHDLGridSource *New();
+  vtkTypeMacro(vtkVelodyneHDLGridSource, vtkPolyDataAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  vtkSetMacro(GridSize, int);
+  vtkGetMacro(GridSize, int);
+
+  vtkSetVector3Macro(Origin, double);
+  vtkGetVector3Macro(Origin, double);
+
+  vtkSetVector3Macro(Normal, double);
+  vtkGetVector3Macro(Normal, double);
+
+  static vtkSmartPointer<vtkPolyData> CreateGrid(int gridSize, double origin[3], double normal[3]);
+
+protected:
+  vtkVelodyneHDLGridSource();
+  ~vtkVelodyneHDLGridSource();
+
+  int RequestData(vtkInformation *,
+                  vtkInformationVector **,
+                  vtkInformationVector *);
+
+  int GridSize;
+  double Origin[3];
+  double Normal[3];
+
+private:
+
+  vtkVelodyneHDLGridSource(const vtkVelodyneHDLGridSource&);
+  void operator = (const vtkVelodyneHDLGridSource&);
+
+};
+#endif
