4a23fdf9bcee5e0dbaff7573fa7032a5b8fc8384
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2013-04-12 06:25:43 -0400

    Fix windows stdint.h issue.

--- VelodyneHDL/vtkPacketFileWriter.h
+++ VelodyneHDL/vtkPacketFileWriter.h
@@ -21,7 +21,15 @@
 
 #include <pcap.h>
 #include <string>
-#include <stdint.h>
+#ifdef _MSC_VER
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#else
+# include <stdint.h>
+#endif
+
 
 class vtkPacketFileWriter
 {
