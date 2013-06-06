d2067a084c71cb727d9910cb5ebb055b0573d0d8
Pat Marion <pat.marion@kitware.com>
2013-06-06 00:38:32 -0400

    Add Apple install rule for documentation pdf.

--- Projects/apple/velodyneviewer.bundle.cmake
+++ Projects/apple/velodyneviewer.bundle.cmake
@@ -7,6 +7,10 @@ include(CPack)
 install(CODE "
      file(INSTALL DESTINATION \"\${CMAKE_INSTALL_PREFIX}\" USE_SOURCE_PERMISSIONS TYPE DIRECTORY FILES
           \"${install_location}/bin/VeloView.app\")
+
+     file(INSTALL DESTINATION \"\${CMAKE_INSTALL_PREFIX}\" USE_SOURCE_PERMISSIONS TYPE FILE FILES
+          \"${SuperBuild_BINARY_DIR}/velodyneviewer/src/velodyneviewer/Doc/VeloView_Developer_Guide.pdf\")
+
      file(WRITE \"\${CMAKE_INSTALL_PREFIX}/VeloView.app/Contents/Resources/qt.conf\"
                 \"\")
      execute_process(
