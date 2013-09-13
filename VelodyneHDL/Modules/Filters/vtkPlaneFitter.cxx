9959124b9fff74b04de1181d7994dc862f7ae0d8
Casey Goodlett <casey.goodlett@kitware.com>
2013-09-13 11:21:33 -0400

    enh: Add module for plane fitting logic

---
+++ VelodyneHDL/Modules/Filters/vtkPlaneFitter.cxx
@@ -0,0 +1,100 @@
+// Copyright 2013 Velodyne Acoustics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkVelodyneHDLReader.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "vtkPlaneFitter.h"
+
+#include "vtkObjectFactory.h"
+#include "vtkPointSet.h"
+#include "vtkSmartPointer.h"
+#include "vtkDoubleArray.h"
+
+#include <Eigen/Dense>
+
+//-----------------------------------------------------------------------------
+vtkStandardNewMacro(vtkPlaneFitter);
+
+//-----------------------------------------------------------------------------
+vtkPlaneFitter::vtkPlaneFitter()
+{
+}
+
+//-----------------------------------------------------------------------------
+vtkPlaneFitter::~vtkPlaneFitter()
+{
+}
+
+//-----------------------------------------------------------------------------
+void vtkPlaneFitter::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
+
+//-----------------------------------------------------------------------------
+void vtkPlaneFitter::PlaneFit(vtkPointSet* pts, double origin[3], double normal[3],
+                              double &minDist, double &maxDist, double &stdDev)
+{
+  using namespace Eigen;
+
+  vtkSmartPointer<vtkDoubleArray> ptdata = vtkSmartPointer<vtkDoubleArray>::New();
+  ptdata->DeepCopy(pts->GetPoints()->GetData());
+
+  const vtkIdType n = ptdata->GetNumberOfTuples();
+
+  assert(ptdata->GetNumberOfComponents() == 3);
+  Map<MatrixXd> eigpoints(static_cast<double*>(ptdata->GetVoidPointer(0)),
+                          ptdata->GetNumberOfTuples(), ptdata->GetNumberOfComponents());
+
+  VectorXd mean(3);
+
+  mean = eigpoints.colwise().sum() / n;
+  assert(mean.size() == 3);
+
+  for(int i = 0; i < 3; ++i)
+    {
+    origin[i] = mean[i];
+    }
+
+  eigpoints.rowwise() -= mean.transpose();
+
+  JacobiSVD<MatrixXd> svd(eigpoints, ComputeThinU | ComputeThinV);
+
+  std::cout << svd.singularValues() << std::endl;
+  std::cout << svd.matrixV() << std::endl;
+
+  VectorXd enormal = svd.matrixV().col(2);
+  assert(enormal.size() == 3);
+
+  for(int i = 0; i < 3; ++i)
+    {
+    normal[i] = enormal[i];
+    }
+
+  minDist = 1;
+  maxDist = 2;
+  stdDev = 1.5;
+}
