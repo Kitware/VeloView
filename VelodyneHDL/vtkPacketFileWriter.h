1a72ae3ebc26ef407a6dfea69ae525df7cc8926b
Casey Goodlett <casey.goodlett@kitware.com>
2013-10-18 11:47:52 -0400

    fix: Refactor vtkPacketFileWriter and add cstdlib include

--- VelodyneHDL/vtkPacketFileWriter.h
+++ VelodyneHDL/vtkPacketFileWriter.h
@@ -34,36 +34,7 @@
 
 #include <pcap.h>
 #include <string>
-#ifdef _MSC_VER
-typedef __int32 int32_t;
-typedef unsigned __int32 uint32_t;
-typedef __int64 int64_t;
-typedef unsigned __int64 uint64_t;
-#else
-# include <stdint.h>
-#endif
-
 #include <vector>
-
-#ifdef _MSC_VER
-
-#include <windows.h>
-
-namespace {
-int gettimeofday(struct timeval * tp, void *)
-{
-  FILETIME ft;
-  ::GetSystemTimeAsFileTime( &ft );
-  long long t = (static_cast<long long>(ft.dwHighDateTime) << 32) | ft.dwLowDateTime;
-  t -= 116444736000000000LL;
-  t /= 10;  // microseconds
-  tp->tv_sec = static_cast<long>( t / 1000000UL);
-  tp->tv_usec = static_cast<long>( t % 1000000UL);
-  return 0;
-}
-}
-
-#endif
 
 class vtkPacketFileWriter
 {
@@ -75,111 +46,25 @@ public:
 
   static const unsigned short PositionPacketHeader[21];
 
-  vtkPacketFileWriter()
-  {
-    this->PCAPFile = 0;
-    this->PCAPDump = 0;
-  }
+  vtkPacketFileWriter();
 
-  ~vtkPacketFileWriter()
-  {
-    this->Close();
-  }
+  ~vtkPacketFileWriter();
 
-  bool Open(const std::string& filename)
-  {
-    this->PCAPFile = pcap_open_dead(DLT_EN10MB, 65535);
-    this->PCAPDump = pcap_dump_open(this->PCAPFile, filename.c_str());
+  bool Open(const std::string& filename);
 
-    if (!this->PCAPDump)
-      {
-      this->LastError = pcap_geterr(this->PCAPFile);
-      pcap_close(this->PCAPFile);
-      this->PCAPFile = 0;
-      return false;
-      }
+  bool IsOpen();
 
-    this->FileName = filename;
-    return true;
-  }
+  void Close();
 
-  bool IsOpen()
-  {
-    return (this->PCAPFile != 0);
-  }
+  const std::string& GetLastError();
 
-  void Close()
-  {
-    if (this->PCAPFile)
-      {
-      pcap_dump_close(this->PCAPDump);
-      pcap_close(this->PCAPFile);
-      this->PCAPFile = 0;
-      this->PCAPDump = 0;
-      this->FileName.clear();
-      }
-  }
+  const std::string& GetFileName();
 
-  const std::string& GetLastError()
-  {
-    return this->LastError;
-  }
+  bool WritePacket(const unsigned char* data, unsigned int dataLength);
 
-  const std::string& GetFileName()
-  {
-    return this->FileName;
-  }
-
-  bool WritePacket(const unsigned char* data, unsigned int dataLength)
-  {
-    if (!this->PCAPFile)
-      {
-      return false;
-      }
-
-    const unsigned short* headerData;
-    struct pcap_pkthdr header;
-
-    std::vector<unsigned char> packetBuffer;
-    if (dataLength == 1206)
-      {
-      headerData = LidarPacketHeader;
-      header.caplen = 1248;
-      header.len = 1248;
-      packetBuffer.resize(1248);
-      }
-    else if(dataLength == (554 - 42))
-      {
-      headerData = PositionPacketHeader;
-      header.caplen = 554;
-      header.len = 554;
-      packetBuffer.resize(554);
-      }
-    else
-      {
-      return false;
-      }
-
-    struct timeval currentTime;
-    gettimeofday(&currentTime, NULL);
-    header.ts = currentTime;
-
-    memcpy(&(packetBuffer[0]), headerData, 42);
-    memcpy(&(packetBuffer[0]) + 42, data, dataLength);
-
-    pcap_dump((u_char *)this->PCAPDump, &header, &(packetBuffer[0]));
-    return true;
-  }
-
-  bool WritePacket(pcap_pkthdr* packetHeader, unsigned char* packetData)
-  {
-    pcap_dump((u_char *)this->PCAPDump, packetHeader, packetData);
-    return true;
-  }
+  bool WritePacket(pcap_pkthdr* packetHeader, unsigned char* packetData);
 
 protected:
-
-
 
   pcap_t* PCAPFile;
   pcap_dumper_t* PCAPDump;
