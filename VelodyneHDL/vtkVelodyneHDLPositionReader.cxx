f06af1fc6a576f1e374c0d63ebb2297cdc734aba
Casey Goodlett <casey.goodlett@kitware.com>
2013-10-23 13:56:33 -0400

    FIX: Fix crash when reading no data

--- VelodyneHDL/vtkVelodyneHDLPositionReader.cxx
+++ VelodyneHDL/vtkVelodyneHDLPositionReader.cxx
@@ -332,7 +332,7 @@ int vtkVelodyneHDLPositionReader::RequestData(vtkInformation *request,
   unsigned int dataLength;
   double timeSinceStart;
 
-  PROJ *pj_utm;
+  PROJ *pj_utm = NULL;
   // PROJ *pj_latlong;
   // const char *latlonargs[4] = {"+proj=longlat", "+ellps=WGS84",
   //                              "+datum=WGS84",  "+no_defs" };
@@ -462,7 +462,10 @@ int vtkVelodyneHDLPositionReader::RequestData(vtkInformation *request,
   output->GetPointData()->AddArray(gpsTime);
   output->GetPointData()->AddArray(times);
 
-  proj_free(pj_utm);
+  if(pj_utm)
+    {
+    proj_free(pj_utm);
+    }
 
   return 1;
 }
