01acf2eeafae839fe4d327fbc91737d7fe2ceba0
Casey Goodlett <casey.goodlett@kitware.com>
2014-08-07 10:00:59 -0400

    build: add iostreams and filesystem

--- Superbuild/Projects/boost.cmake
+++ Superbuild/Projects/boost.cmake
@@ -2,7 +2,7 @@ add_external_project_or_use_system(boost
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND
     <SOURCE_DIR>/bootstrap.sh --prefix=<INSTALL_DIR>
-                              --with-libraries=date_time,thread,regex,system,program_options
+                              --with-libraries=date_time,thread,regex,system,program_options,filesystem,iostreams
   BUILD_COMMAND <SOURCE_DIR>/bjam address-model=64 threading=multi
   INSTALL_COMMAND <SOURCE_DIR>/bjam  address-model=64 threading=multi --prefix=<INSTALL_DIR> install
 )
