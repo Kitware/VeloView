dd8e3492321c335b4cf009ca727f75ce9c30ea4c
Matthew Woehlke <matthew.woehlke@kitware.com>
2014-10-02 15:35:59 -0400

    remove specification of LIST_SEPARATOR
    We're not entirely sure why this was needed originally, but it confuses
    the heck out of Ninja. For now, just remove it; if it turns out it's
    useful, we'll revisit.

--- Superbuild/Projects/paraview.cmake
+++ Superbuild/Projects/paraview.cmake
@@ -39,6 +39,4 @@ add_external_project(paraview
     # specify the apple app install prefix. No harm in specifying it for all
     # platforms.
     -DMACOSX_APP_INSTALL_PREFIX:PATH=<INSTALL_DIR>/Applications
-
-  LIST_SEPARATOR +
 )
