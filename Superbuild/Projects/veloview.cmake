f6f3b6929fe9d3e5d4f981c2768359c9ff1614ab
Casey Goodlett <casey.goodlett@kitware.com>
2014-11-17 13:43:32 -0500

    Fix path to eigen during superbuild

--- Superbuild/Projects/veloview.cmake
+++ Superbuild/Projects/veloview.cmake
@@ -4,6 +4,6 @@ add_external_project(veloview
   CMAKE_ARGS
     -DBUILD_SHARED_LIBS:BOOL=ON
     -DParaView_DIR:PATH=${SuperBuild_BINARY_DIR}/paraview/src/paraview-build
-    -DEIGEN_INCLUDE_DIR:PATH=<INSTALL_DIR>/eigen
+    -DEIGEN_INCLUDE_DIR:PATH=<INSTALL_DIR>/include/eigen3
     -DPYTHONQT_DIR:PATH=<INSTALL_DIR>
 )
