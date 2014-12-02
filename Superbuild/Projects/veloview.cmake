b516f2e4de3c10caf5d93ef49975dfdb7a4b53e0
Casey Goodlett <casey.goodlett@kitware.com>
2014-12-02 10:09:29 -0500

    Revert "Use superbuild by default"
    This reverts commit 59e79f25aea1c65a0094fa437f55a7be0dcfff32.

--- Superbuild/Projects/veloview.cmake
+++ Superbuild/Projects/veloview.cmake
@@ -3,7 +3,6 @@ add_external_project(veloview
 
   CMAKE_ARGS
     -DBUILD_SHARED_LIBS:BOOL=ON
-    -DUSE_SUPERBUILD:BOOL=OFF
     -DParaView_DIR:PATH=${SuperBuild_BINARY_DIR}/paraview/src/paraview-build
     -DEIGEN_INCLUDE_DIR:PATH=<INSTALL_DIR>/include/eigen3
     -DPYTHONQT_DIR:PATH=<INSTALL_DIR>
