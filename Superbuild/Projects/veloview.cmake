6926452eaf4e87e0fa1ae4d9caefa895fc4f354f
Bastien Jacquet <bastien.jacquet@kitware.com>
2016-07-28 19:48:47 +0200

    Hard-code VeloView's VTK dir to Paraview's one

--- Superbuild/Projects/veloview.cmake
+++ Superbuild/Projects/veloview.cmake
@@ -6,4 +6,5 @@ add_external_project(veloview
     -DParaView_DIR:PATH=${SuperBuild_BINARY_DIR}/paraview/src/paraview-build
     -DEIGEN_INCLUDE_DIR:PATH=<INSTALL_DIR>/include/eigen3
     -DPYTHONQT_DIR:PATH=<INSTALL_DIR>
+    -DVTK_DIR:PATH=${SuperBuild_BINARY_DIR}/paraview/src/paraview-build/VTK
 )
