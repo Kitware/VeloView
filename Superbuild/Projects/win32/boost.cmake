2a1efb699e148e9a724e70bbbead3c6b95a29111
Bastien Jacquet <bastien.jacquet@kitware.com>
2016-07-28 18:45:03 +0200

    Let boost auto-detect msvc toolset version

--- Superbuild/Projects/win32/boost.cmake
+++ Superbuild/Projects/win32/boost.cmake
@@ -9,6 +9,6 @@ add_external_project_or_use_system(boost
   CONFIGURE_COMMAND
     <SOURCE_DIR>/bootstrap.bat
   BUILD_COMMAND <SOURCE_DIR>/b2
-    toolset=msvc-9.0 address-model=${boost_address_model} --prefix=<INSTALL_DIR> --with-regex --with-system --with-date_time --with-program_options --with-iostreams --with-filesystem --with-thread install
+    address-model=${boost_address_model} --prefix=<INSTALL_DIR> --with-regex --with-system --with-date_time --with-program_options --with-iostreams --with-filesystem --with-thread install
   INSTALL_COMMAND ""
 )
