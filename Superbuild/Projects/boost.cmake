7f06b20be33be2c6068c023cdb383b40a393f0f4
Bastien Jacquet <bastien.jacquet@kitware.com>
2016-11-23 10:31:56 +0100

    Fixup boost
    libelous actually needs libboost_chrono

--- Superbuild/Projects/boost.cmake
+++ Superbuild/Projects/boost.cmake
@@ -2,7 +2,7 @@ add_external_project_or_use_system(boost
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND
     <SOURCE_DIR>/bootstrap.sh --prefix=<INSTALL_DIR>
-                              --with-libraries=date_time,thread,regex,system,program_options,filesystem,iostreams
+                              --with-libraries=date_time,thread,regex,system,program_options,filesystem,iostreams,chrono
   BUILD_COMMAND <SOURCE_DIR>/bjam address-model=${VV_BUILD_ARCHITECTURE} threading=multi
   INSTALL_COMMAND <SOURCE_DIR>/bjam  address-model=${VV_BUILD_ARCHITECTURE} threading=multi --prefix=<INSTALL_DIR> install
 )
