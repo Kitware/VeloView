9bd9403fec9ea1acec462178920ab667cd8d3fbd
Louis Amore <louis.amore@kitware.com>
2017-03-17 16:22:21 +0100

    Create the grid & RPM before the sensor
    Doing this allows the sensor to be the active source

--- VelodyneHDL/python/veloview/applogic.py
+++ VelodyneHDL/python/veloview/applogic.py
@@ -404,6 +404,9 @@ def openSensor():
     ipAddressForwarding = calibration.ipAddressForwarding
 
     close()
+    app.grid = createGrid()
+
+    initializeRPMText()
 
     sensor = smp.VelodyneHDLSource(guiName='Data', CalibrationFile=calibrationFile, CacheSize=100)
     sensor.GetClientSideObject().SetLIDARPort(LIDARPort)
@@ -443,11 +446,8 @@ def openSensor():
     app.actions['actionShowRPM'].enabled = True
     app.actions['actionCorrectIntensityValues'].enabled = True
 
-    initializeRPMText()
-
     #Auto adjustment of the grid size with the distance resolution
     app.distanceResolutionM = sensor.GetClientSideObject().GetDistanceResolutionM()
-    app.grid = createGrid()
     app.actions['actionMeasurement_Grid'].setChecked(True)
     showMeasurementGrid()
 
