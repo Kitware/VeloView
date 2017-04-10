eaa7bf7f531fe07797d51d24645d755a49bdd473
Laurent Bardoux <laurent.bardoux@kitware.com>
2017-04-10 11:09:21 +0200

    Disabling the select dual return menu at the beginning

--- VelodyneHDL/python/veloview/applogic.py
+++ VelodyneHDL/python/veloview/applogic.py
@@ -1776,6 +1776,7 @@ def start():
     setupActions()
     disablePlaybackActions()
     disableSaveActions()
+    app.actions['actionSelectDualReturn'].setEnabled(False)
     app.actions['actionMeasure'].setEnabled(view.CameraParallelProjection)
     setupStatusBar()
     setupTimeSliderWidget()
