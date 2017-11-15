b5332161d0b5040fb0198707c885573ef7fd20ff
Pierre Guilbert <pierre.guilbert@kitware.com>
2017-11-15 13:18:29 +0100

    bugfix : the last built-in .xml file can be removed, and should not

--- VelodyneHDL/vvCalibrationDialog.cxx
+++ VelodyneHDL/vvCalibrationDialog.cxx
@@ -525,7 +525,7 @@ void vvCalibrationDialog::accept()
 //-----------------------------------------------------------------------------
 void vvCalibrationDialog::onCurrentRowChanged(int row)
 {
-  this->Internal->RemoveButton->setEnabled(row >= this->Internal->BuiltInCalibrationFiles.size());
+  this->Internal->RemoveButton->setEnabled(row > this->Internal->BuiltInCalibrationFiles.size());
 }
 
 //-----------------------------------------------------------------------------
@@ -556,7 +556,7 @@ void vvCalibrationDialog::addFile()
 void vvCalibrationDialog::removeSelectedFile()
 {
   const int row = this->Internal->ListWidget->currentRow();
-  if (row >= this->Internal->BuiltInCalibrationFiles.size())
+  if (row > this->Internal->BuiltInCalibrationFiles.size())
   {
     delete this->Internal->ListWidget->takeItem(row);
     this->Internal->saveFileList();
