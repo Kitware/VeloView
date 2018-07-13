d223121a9ba0a9221907471c6b496e8ab26e1fdd
Nick Laurenson <nick.laurenson@kitware.com>
2018-07-13 06:08:37 -0400

    Fix some bug in Continuous Integration

--- CI/findRightSuperbuildCache.sh
+++ CI/findRightSuperbuildCache.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# This script enable to find the cache superbuild for this branch: 
+# This script enable to find the cache superbuild for this branch:
 # - fisrt: the branch specific superbuild
 # - then: a parent/master branch specific superbuild
 # - finally: the kitware-master branch superbuild
 #
 # Let's give you a example to be more specific:
-# Suppose my branch name is feature/Slam/.../test/Nick and with try 
+# Suppose my branch name is feature/Slam/.../test/Nick and with try
 # to compile under os
 # first:
 #   ---> os-feature/Slam/.../test/Nick
@@ -27,24 +27,36 @@ MINIO_CACHE_SERVER_SECRET_KEY=$5
 
 prefix=superbuild/runner/minio/project/808
 
+# start timer
+START_TIME=$SECONDS
+
 # register cache server
 ~/mc config host add superbuild ${MINIO_CACHE_SERVER_ADDRESS} ${MINIO_CACHE_SERVER_ACCESS_KEY} ${MINIO_CACHE_SERVER_SECRET_KEY}
+
 # check for the right superbuild
-path=${OS}-${CI_COMMIT_REF}
+path=${OS}/${CI_COMMIT_REF}
 # found the right superbuild
 superbuild_cache=$(~/mc find superbuild --name ${path}"-1")
 while [ "$superbuild_cache" == "" ]
     do
+    echo "check if there exist a specific superbuild for: " $path
     # check if a superbuild exist
     superbuild_cache=$(~/mc find superbuild ${prefix}/${path}/master-1 | grep ${prefix}/${path}/master-1)
-    echo "my path" $path
     if [ "$path" == "." ]; then
-        superbuild_cache=$(~/mc find superbuild ${prefix}/${OS}-kitware-master-1 | grep ${prefix}/${path}/-kitware-master-1)  
+        superbuild_cache=$(~/mc find superbuild ${prefix}/${OS}-kitware-master-1 | grep ${prefix}/${OS}-kitware-master-1)
+        if [ "$superbuild_cache" == "" ]; then
+            echo "!!!WARNING!!! No default superbuild :" ${OS} "-kitware-master exist, please run a superbuild job for the kitware master or current branch manually via the gitlab interface"
+            exit
+        fi
     fi
     # get parent dir
     path=`dirname -- ${path}`
 done
-echo "superbuild: " $superbuild_cache
+echo "The cached superbuild: " $superbuild_cache " will be used"
+
+# download and unzip the superbuild
 mkdir cache
 ~/mc cp --recursive $superbuild_cache cache
-unzip cache/* 
+unzip -q cache/*
+
+echo "Getting the cached superbuild took " $(($SECONDS - $START_TIME)) "s"
