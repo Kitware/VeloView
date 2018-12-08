126d14bcf663b5266c8e36303fe91f34228e48fa
Nick Laurenson <nick.laurenson@kitware.com>
2018-12-08 20:48:26 +0100

    add toolbar to MainWindow and remove the old one

--- vvMainWindow.cxx
+++ vvMainWindow.cxx
@@ -61,6 +61,7 @@
 #include <pqVelodyneManager.h>
 #include <pqParaViewMenuBuilders.h>
 #include <pqTabbedMultiViewWidget.h>
+#include <pqSetName.h>
 #include <vtkPVPlugin.h>
 #include <vtkSMPropertyHelper.h>
 
@@ -77,6 +78,7 @@
 #include <sstream>
 
 #include "vvConfig.h"
+#include "vvPlayerControlsToolbar.h"
 
 // Declare the plugin to load.
 PV_PLUGIN_IMPORT_INIT(VelodyneHDLPlugin);
@@ -112,6 +114,10 @@ private:
   {
     pqApplicationCore* core = pqApplicationCore::instance();
 
+    // need to be created before the first scene
+    QToolBar* vcrToolbar = new vvPlayerControlsToolbar(window)
+      << pqSetName("Player Control");
+
     // Register ParaView interfaces.
     pqInterfaceTracker* pgm = core->interfaceTracker();
     //    pgm->addInterface(new pqStandardViewModules(pgm));
@@ -126,6 +132,7 @@ private:
     //    new pqDefaultViewBehavior(window);
     new pqCrashRecoveryBehavior(window);
     new pqAutoLoadPluginXMLBehavior(window);
+    new pqDataTimeStepBehavior(window);
     new pqCommandLineOptionsBehavior(window);
     pqApplyBehavior* applyBehaviors = new pqApplyBehavior(window);
 
@@ -267,6 +274,9 @@ private:
     this->Ui.spreadSheetDock->hide();
     this->Ui.informationDock->hide();
     this->Ui.memoryInspectorDock->hide();
+
+    // create toolbar
+    window->addToolBar(Qt::TopToolBarArea, vcrToolbar);
 
     // Setup the View menu. This must be setup after all toolbars and dockwidgets
     // have been created.
