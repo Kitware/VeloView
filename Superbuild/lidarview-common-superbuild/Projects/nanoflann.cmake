31fcd7b5fa2994a600844abe4c3a675be1fb61ef
Nick Laurenson <nick.laurenson@kitware.com>
2019-11-25 18:11:42 +0100

    Move Superbuild to submodule

--- Superbuild/lidarview-common-superbuild/Projects/nanoflann.cmake
+++
@@ -1,9 +0,0 @@
-superbuild_add_project(nanoflann
-  DEPENDS eigen
-
-  CMAKE_ARGS
-  -DBUILD_EXAMPLES=OFF
-  -DBUILD_TESTS=OFF
-  -DEIGEN3_DIR=<INSTALL_DIR>/include/eigen3
-  -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>/nanoflann
-  )
--- Superbuild/lidarview-common-superbuild/Projects/pcap.cmake
+++
@@ -1,7 +0,0 @@
-superbuild_add_project(pcap
-  BUILD_IN_SOURCE 1
-  CONFIGURE_COMMAND <SOURCE_DIR>/configure
-                    --prefix=<INSTALL_DIR>
-  BUILD_COMMAND make
-  CAN_USE_SYSTEM
-)
