b85f3867cbd506947426ceb38e0b5de39bcf1c64
Arnaud Billon <arnaud.billon@kitware.com>
2021-07-08 13:56:14 +0200

    [upgrade] Remove unused VTK_DIR

--- Superbuild/Projects/lidarview.cmake
+++ Superbuild/Projects/lidarview.cmake
@@ -13,7 +13,6 @@ superbuild_add_project(lidarview
     -DSOFTWARE_VENDOR=${SOFTWARE_VENDOR}
     -Dsuperbuild_python_version=${superbuild_python_version}
     -DParaView_DIR:PATH=${SuperBuild_BINARY_DIR}/common-superbuild/paraview/build
-    -DVTK_DIR:PATH=${SuperBuild_BINARY_DIR}/common-superbuild/paraview/build/VTK
     -DCMAKE_CXX_STANDARD:STRING=${CMAKE_CXX_STANDARD}
     -Dparaview_version=${paraview_version}
     -Dqt_version:STRING=${qt_version}
