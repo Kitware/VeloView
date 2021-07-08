d1957846a9154c1170e3dca8a01f089c5b49fb68
Arnaud Billon <arnaud.billon@kitware.com>
2021-07-08 13:54:16 +0200

    [applogic] Fix Import Lidarplugin/VelodynePlugin modules

--- Application/Ui/python/lidarview/applogic.py
+++ Application/Ui/python/lidarview/applogic.py
@@ -37,7 +37,7 @@ from PythonQt.paraview import vvCalibrationDialog, vvCropReturnsDialog, vvSelect
 # import the vtk wrapping of the Lidar Plugin
 # this enable to get the specific vtkObject behind a proxy via GetClientSideObject()
 # without this plugin, GetClientSideObject(), would return the first mother class known by paraview
-import LidarPluginPython
+import LidarPlugin.LidarCore
 
 import lidarview.planefit as planefit
 
